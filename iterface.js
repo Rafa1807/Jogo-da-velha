df5405d9a4812637e345647334ad8b77f2986dd7
Rafa1807 <rafaelribeirobernardes@bol.com.br>
2021-09-26 11:52:46 -0300

    Projeto jogo da velha, com botao de reset

---
+++ iterface.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick)
+    })
+
+} )
+
+function handleClick(event){
+    
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)) {
+        
+        setTimeout(() => {
+
+            alert(" O jogo Acabou ");
+
+        }, 5)
+        
+    };
+    updateSquares();
+
+}
+
+function updateSquares() {
+    
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square)=>{
+        let position = square.id;
+        let symbol = board[position]
+
+        if(symbol != '') {
+            square.innerHTML = `<div class='${symbol}'></div>`
+        }
+    })
+}
